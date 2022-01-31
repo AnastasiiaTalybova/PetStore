f976fdbbf0673f51184ac047a9a9750ece28bf55
Anastasiia Talybova <talybova.an@gmail.com>
2022-01-31 19:27:26 +0200

    Add multi-reporter

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,9 +19,9 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 import addContext from "mochawesome/addContext";
-Cypress.on("test:after:run", (test, runnable) => {  
-  if (test.state === "failed") {    
-    const screenshot =`assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;    
-    addContext({ test }, screenshot);  
+Cypress.on("test:after:run", (test, runnable) => {
+  if (test.state === "failed") { 
+    const screenshot       =`assets/${Cypress.spec.name}/${runnable.parent.title} --       ${test.title} (failed).png`;
+addContext({ test }, screenshot);
   }
 });
